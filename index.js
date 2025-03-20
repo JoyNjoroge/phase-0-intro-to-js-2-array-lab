5665268c5a5cce53d33d2c691b9bbbd42c035547
Joy Njoroge <njorogejoy78@gmail.com>
2025-03-20 15:52:19 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+let cats = [];
+cats.length = 0;
+cats.push('Milo','Otis','Garfield' );
+
+
+// Array Functions
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+
+function destructivelyPrependCat(name){
+    return cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    return cats.shift();
+}
+
+function appendCat() {
+    let newCat = [...cats, 'Broom'];
+    return newCat;
+}
+console.log(appendCat());
+
+function prependCat() {
+    let newCats = ['Arnold', ...cats];
+    return newCats;
+}
+console.log(prependCat());
+
+function removeLastCat(){
+    let newCatsNoLastCat = cats.slice(0, -1);
+    return newCatsNoLastCat;
+}
+console.log(removeLastCat());
+
+function removeFirstCat() {
+    let newCatsNoFirstCat = cats.slice(1);
+    return newCatsNoFirstCat;
+}
+console.log(removeFirstCat());
